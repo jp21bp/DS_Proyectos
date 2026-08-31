f9911b0a03894b8a76d06c13392ad133f1cdc3e0
JP <jowapra@gmail.com>
2026-08-31 16:32:58 -0500

    Cleaned data

--- 2_OptimizacionPortafolio/cleaning.py
+++ 2_OptimizacionPortafolio/cleaning.py
@@ -17,21 +17,39 @@ data_path = os.path.join(
 df = pd.read_csv(
     f'{data_path}/raw_splac.csv',
     parse_dates=['Date'],
-    index_col='Date',
+    index_col='Date'
 )
 
-df.index
-##### 
-df.info()
-df.columns.nlevels
-df.head()
-
-df['Date'].dtype
 
 #######################################
     # Nulls #
+#### Forward fill (for weekends) before full null-analysis
+df = df.ffill()
+
+#### Capturing 2008 crisis and build up
+    # Erasing all companies with NaN after 2006-3-31
+df_erase = df[df.index > "2006-3-31"].isna().sum()
+df = df.drop(columns=df_erase[df_erase > 0].index.to_list())
+
+#### Creating dataframe where ALL rows have some value
+df = df[df.notna().all(axis=1)]
+
+##########################################
+    # Duplicates #
+#### Counting number of duplicates
+df[df.duplicated()].shape
+    # There are 47 repeated rows
+
+#### Delete duplicates 
+df = df.drop_duplicates()
 
 
+##########################################
+    # Saving #
+df.to_csv(
+    f'{data_path}/clean_splac.csv',
+    index=True,
+    encoding='utf-8')
 
 
 
