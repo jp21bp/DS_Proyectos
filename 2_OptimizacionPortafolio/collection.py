a186d8cf3289dd9f9dd6f0f5f2e1171970d415dd
JP <jowapra@gmail.com>
2026-09-02 09:45:28 -0500

    Download of all data

--- 2_OptimizacionPortafolio/collection.py
+++ 2_OptimizacionPortafolio/collection.py
@@ -27,11 +27,11 @@ df_splac_raw = splac_tick.history(interval='1d', start = end_date, end=end_date)
 
 ##### SAve only 'Adj Close'
 df_splac_AC = df_splac_raw.xs('Adj Close', axis=1, level=0)
-df_splac_AC.to_csv(f'{data_path}/Data/raw_splac.csv', index=True, encoding='utf-8')
+df_splac_AC.to_csv(f'{data_path}/Data/raw_splac.csv', index=True, encoding='utf-8', multi_level_index=False)
 
 ##### Alternative, if above doesn't work
     # Go to Google Sheets and PLace the following command
-    # =GOOGLEFINANCE("INDEXSP:SPLAC", "price", "2000-01-01", "2026-07-31", "DAILY")
+    # =GOOGLEFINANCE("INDEXSP:SPLAC", "all", "2000-01-01", "2026-07-31", "DAILY")
 
 #######################################################################
     # Top 40 Inside SPLAC #
@@ -46,8 +46,12 @@ start_date = "2000-01-01"
     #Launch data = September 30, 1999
 end_date = "2026-07-31"
     #Top 40 recorded on 7/31/26
-df_raw = yf.download(tickers, start=start_date, end=end_date, auto_adjust=False)
+df_raw = yf.download(tickers, start=start_date, end=end_date, auto_adjust=False, multi_level_index=False)
 
 ##### Filter to only save 'Adj Close' and dates
 df_raw_AC = df_raw.xs('Adj Close', axis=1, level=0)
 df_raw_AC.to_csv(f'{data_path}/Data/raw_top40.csv', index=True, encoding='utf-8')
+
+##### Saving all original multi level columns
+df_raw.columns = ['_'.join(col).strip() for col in df_raw.columns.values]
+df_raw.to_csv(f'{data_path}/Data/raw_top40_complete.csv', index=True, encoding='utf-8')
