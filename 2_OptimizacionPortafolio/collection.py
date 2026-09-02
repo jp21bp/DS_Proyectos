dcb1892d97ab59d162c7de88b4a8fdb4af1c8283
JP <jowapra@gmail.com>
2026-09-02 16:53:50 -0500

    Downloaded top 40 from August and cleaned it

--- 2_OptimizacionPortafolio/collection.py
+++ 2_OptimizacionPortafolio/collection.py
@@ -14,37 +14,16 @@ data_path = os.path.join(
 )
 
 #######################################################################
-    # SPLAC index #
-##### Getting the ticker
-splac_tick =  yf.Ticker("^SPLAC")
-
-##### Download and Collection
-start_date = "2000-07-30"
-    #Launch data = September 30, 1999
-end_date = "2026-07-31"
-    #Top 40 recorded on 7/31/26
-df_splac_raw = splac_tick.history(interval='1d', start = end_date, end=end_date)
-
-##### SAve only 'Adj Close'
-df_splac_AC = df_splac_raw.xs('Adj Close', axis=1, level=0)
-df_splac_AC.to_csv(f'{data_path}/Data/raw_splac.csv', index=True, encoding='utf-8', multi_level_index=False)
-
-##### Alternative, if above doesn't work
-    # Go to Google Sheets and PLace the following command
-    # =GOOGLEFINANCE("INDEXSP:SPLAC", "all", "2000-01-01", "2026-07-31", "DAILY")
-
-#######################################################################
     # Top 40 Inside SPLAC #
 ##### Tickers
 with open(f'{data_path}/SPLAC_tickers.txt', 'r', encoding='utf-8') as file:
     tickers = file.readlines()
-tickers = sorted([elem.rstrip('\n') for elem in tickers])
 
 
 ##### Download and Collection
 start_date = "2000-01-01"
     #Launch data = September 30, 1999
-end_date = "2026-07-31"
+end_date = "2026-08-31"
     #Top 40 recorded on 7/31/26
 df_raw = yf.download(tickers, start=start_date, end=end_date, auto_adjust=False)
 
@@ -55,3 +34,24 @@ df_raw_AC.to_csv(f'{data_path}/Data/raw_top40.csv', index=True, encoding='utf-8'
 ##### Saving all original multi level columns
 df_raw.columns = ['_'.join(col).strip() for col in df_raw.columns.values]
 df_raw.to_csv(f'{data_path}/Data/raw_top40_complete.csv', index=True, encoding='utf-8')
+
+
+#######################################################################
+    # SPLAC index #
+##### Getting the ticker
+splac_tick =  yf.Ticker("^SPLAC")
+
+##### Download and Collection
+start_date = "2000-07-30"
+    #Launch data = September 30, 1999
+end_date = "2026-08-31"
+    #Top 40 recorded on 7/31/26
+df_splac_raw = splac_tick.history(interval='1d', start = end_date, end=end_date)
+
+##### SAve only 'Adj Close'
+df_splac_AC = df_splac_raw.xs('Adj Close', axis=1, level=0)
+df_splac_AC.to_csv(f'{data_path}/Data/raw_splac_prices.csv', index=True, encoding='utf-8', multi_level_index=False)
+
+##### Alternative, if above doesn't work
+    # Go to Google Sheets and PLace the following command
+    # =GOOGLEFINANCE("INDEXSP:SPLAC", "all", "2000-01-01", "2026-08-31", "DAILY")
---
+++ 2_OptimizacionPortafolio/portfolio_strats.py
@@ -0,0 +1,14 @@
+"""
+This file contains the following from the original paper:
+* Non-ML portoflio strategies
+* Performance Metrics
+"""
+
+
+
+
+
+
+
+
+
